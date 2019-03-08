5f2ff5cd71322a2b7d9050cd40071cc856de562a
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-08 10:58:17 +0500

    Exceptions

---
+++ src/Exception/FailSafeValidateException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "cryptolabs-pk/bip32-keypair-php" package.
+ * https://github.com/cryptolabs-pk/bip32-keypair-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/cryptolabs-pk/bip32-keypair-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace CryptoLabs\BIP32\Exception;
+
+/**
+ * Class FailSafeValidateException
+ * @package CryptoLabs\BIP32\Exception
+ */
+class FailSafeValidateException extends KeyPairException
+{
+}
